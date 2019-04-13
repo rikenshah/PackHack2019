12443cdb18eb6f8258a3458e26330cf92e95306c
rikenshah <rikenmanojshah@gmail.com>
2019-04-13 11:32:14 -0400

    Automated Commit

--- CameraButton.py
+++ CameraButton.py
@@ -6,7 +6,9 @@ camera = PiCamera()
 
 button = Button(2)
 
-camera.start_preview()
+camera.start_preview(alpha=200)
+
 button.wait_for_press()
+
 camera.capture('/images/buttonrpi.jpg')
 camera.stop_preview()
