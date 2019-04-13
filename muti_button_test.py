105a87f0281a4cb4874d1c997134ff4bfd2cc975
daxamin <daxkamin@gmail.com>
2019-04-13 15:17:44 -0400

    Bug Fix

--- muti_button_test.py
+++ muti_button_test.py
@@ -1,18 +1,19 @@
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN)
-GPIO.setup(18, GPIO.IN)
-GPIO.setup(15, GPIO.IN)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while True:
     print(".", end='')
 # GPIO.output(23, True)
+    print(str(GPIO.input(17)) + "----------")
     if(GPIO.input(17) == False):
         print ("Pin 17 / Green Button is true")
         # do stuff based on pin 25 here
-    elif(GPIO.input(18) == False):
+    """elif(GPIO.input(18) == False):
         print ("Pin 18 / Yellow Button is true")
         # do stuff based on pin 18 here
     elif(GPIO.input(15) == False):
         print ("Pin 15 / Red Button is true")
-        # and again for pin 22
+        # and again for pin 22"""
