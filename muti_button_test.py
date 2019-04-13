4466a0ada679231b1637f464cdff12a095258553
daxamin <daxkamin@gmail.com>
2019-04-13 15:26:17 -0400

    Bug Fix

--- muti_button_test.py
+++ muti_button_test.py
@@ -1,19 +1,26 @@
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# import RPi.GPIO as GPIO
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+def green_pressed:
+    print("LOLOLO")
+from gpiozero import Button
+
+button_green = Button(17)
 
 while True:
-    # print(".", end='')
-# GPIO.output(23, True)
-    # print(str(GPIO.input(17)) + "----------")
-    if(GPIO.input(17)):
-        print ("Pin 17 / Green Button is true")
-        # do stuff based on pin 25 here
-    """elif(GPIO.input(18) == False):
-        print ("Pin 18 / Yellow Button is true")
-        # do stuff based on pin 18 here
-    elif(GPIO.input(15) == False):
-        print ("Pin 15 / Red Button is true")
-        # and again for pin 22"""
+    button_green.when_pressed(green_pressed)
+#     # print(".", end='')
+# # GPIO.output(23, True)
+#     # print(str(GPIO.input(17)) + "----------")
+#     if(GPIO.input(17)):
+#         print ("Pin 17 / Green Button is true")
+#         # do stuff based on pin 25 here
+#     """elif(GPIO.input(18) == False):
+#         print ("Pin 18 / Yellow Button is true")
+#         # do stuff based on pin 18 here
+#     elif(GPIO.input(15) == False):
+#         print ("Pin 15 / Red Button is true")
+#         # and again for pin 22"""
+
