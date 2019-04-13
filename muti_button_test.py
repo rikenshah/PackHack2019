be443814aa947d16793f12c70323795814a70420
daxamin <daxkamin@gmail.com>
2019-04-13 15:11:24 -0400

    Bug Fix

--- muti_button_test.py
+++ muti_button_test.py
@@ -6,13 +6,13 @@ GPIO.setup(15, GPIO.IN)
 
 while True:
     print(".", end='')
-#    GPIO.output(23, True)
-   if(GPIO.input(17) == True):
-      print ("Pin 17 / Green Button is true")
-      # do stuff based on pin 25 here
-   elif(GPIO.input(18) == True):
-      print ("Pin 18 / Yellow Button is true")
-      # do stuff based on pin 18 here
-   elif(GPIO.input(15) == True):
-      print ("Pin 15 / Red Button is true")
-      # and again for pin 22
+# GPIO.output(23, True)
+    if(GPIO.input(17) == True):
+        print ("Pin 17 / Green Button is true")
+        # do stuff based on pin 25 here
+    elif(GPIO.input(18) == True):
+        print ("Pin 18 / Yellow Button is true")
+        # do stuff based on pin 18 here
+    elif(GPIO.input(15) == True):
+        print ("Pin 15 / Red Button is true")
+        # and again for pin 22
