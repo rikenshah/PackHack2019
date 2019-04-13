de1eaa4a8ab84d5991e46cfe28a5460f92533ab2
daxamin <daxkamin@gmail.com>
2019-04-13 15:05:26 -0400

    Bug Fix

--- muti_button_test.py
+++ muti_button_test.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
